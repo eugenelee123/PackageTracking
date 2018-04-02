af5e035b83c797d4c60641f8959404032d1af739
marshall229 <36282437+marshall229@users.noreply.github.com>
2018-04-01 18:38:10 -0700

    Update PackageTracking.cpp

--- PackageTracking.cpp
+++ PackageTracking.cpp
@@ -7,70 +7,167 @@
 //
 
 #include "PackageTracking.h"
+#include <iostream>
+#include <string>
 
 PackageTracking::PackageTracking(const string& strnum) {
-//to be completed
+	
 }
 
 // add a new update
-void PackageTracking::m_addUpdate( const string& status, const string& location, const time_t& timeUpdated){
-//to be completed
+void PackageTracking::m_addUpdate(const string& status, const string& location, const time_t& timeUpdated) 
+{
+	ShippingStatus s(status, location, timeUpdated);
+	mylist.push_front(s); 
+	n++;
 }
 
 bool PackageTracking::m_moveBackward()//move iterator one step earlier in time
 {
-//to be completed
+	//to be completed
+	if (mylist.empty() || temp == mylist.begin())
+	{
+		return false;
+	}
+
+	else
+	{
+		temp--;
+		return true;
+	}
+	
+	
 }
 
 bool PackageTracking::m_moveForward()//move iterator one step forward in time
 {
-//to be completed
+	//to be completed
+	if (mylist.empty() || temp == mylist.end())
+		return false;
+	else
+	{
+		temp++;
+		return true;
+	}
 }
 
-string PackageTracking::m_getLocation( )//return the location of the current update
+string PackageTracking::m_getLocation()//return the location of the current update
 {
-//to be completed
+	//to be completed
+	return temp->_location;
 }
 
-time_t PackageTracking::m_getTime( )//return the time of the current update
+time_t PackageTracking::m_getTime()//return the time of the current update
 {
-//to be completed
+	//to be completed
+	return temp->_time;
 }
 
-string PackageTracking::m_getStatus( )//return the status of the current update
+string PackageTracking::m_getStatus()//return the status of the current update
 {
-//to be completed
+	//to be completed
+	return temp->_status;
 }
 
 int PackageTracking::m_getNumofUpdate() const // get the total numbers of shipping status updates
 {
-//to be completed
+	//to be completed
+	return n;
+
 }
 
 void PackageTracking::m_printPreviousUpdates() //print all previous updates in the shipping chain when the package was shipped, all the way up to (but not including) the current update you are viewing (may not be the most recent update)
-{	
-//to be completed
+{
+	//to be completed
+	while (temp != mylist.begin())
+	{
+		temp--;
+		cout << "Status: " << m_getStatus() << " Location: " << m_getLocation() << "Time: " << m_getTime() << endl;
+	}
+
 }
 
 //print all updates from the current update you are viewing to the last update in the tracking chain
 void PackageTracking::m_printFollowingUpdates()
 {
-//to be completed
+	//to be completed
+	while (temp != mylist.end())
+	{
+		temp++;
+		cout << "Status: " << m_getStatus() << " Location: " << m_getLocation() << "Time: " << m_getTime() << endl;
+	}
+
 }
 
 void PackageTracking::m_printFullTracking()//print all the updates in the tracking chain.
 {
-//to be completed
+	//to be completed
+	list<ShippingStatus>::iterator it = mylist.begin();
+	while (it != mylist.end())
+	{
+		cout << "Status: " << it->_status << "Location: " << it->_location << "Time: " << it->_time << endl;
+		it++;
+	}
+
 }
 
 bool PackageTracking::m_setCurrent(const time_t& timeUpdated)//view an update.
 {
-//to be completed
+	//to be completed
+	list<ShippingStatus>::iterator it = mylist.begin();
+	while (it != mylist.end())
+	{
+		if (timeUpdated == it->_time)
+		{
+			cout << "Status:" << it->_status << " Location:" << it->_location << " Time:" << it->_time << endl;
+			return true;
+		}
+		else
+			it++;
+	}
+	return false;
 }
 
 
-bool PackageTracking::m_readTrackingFile(string fileName) {
-//to be completed
+bool PackageTracking::m_readTrackingFile(string fileName) 
+{
+	string line;
+	ifstream myfile(fileName);
+	string location;
+	string status;
+	time_t time;
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			if (line == "new")
+			{
+				getline(myfile, status, ';');
+				getline(myfile, location, ';');
+				myfile >> time;
+				cout << "Your status is: " << status << endl;
+				cout << "Your location is: " << location << endl;
+
+				cout << "Your time is: " << time << endl << endl;
+				m_addUpdate(status, location, time);
+			}
+
+			else if (line == "forward")
+			{
+				m_moveForward();
+			}
+
+			else if (line == "back")
+			{
+				m_moveBackward();
+			}
+
+		}
+
+		myfile.close();
+		return true;
+	}
+
+	else
+		return false;
 }
-
-
